e0b162f81b99e7ff2970106f893008c393a1f69f
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-02-13 01:36:36 -0700

    delete menu capability

--- js/Favorites.mjs
+++ js/Favorites.mjs
@@ -10,6 +10,7 @@ export default class Favorites{
         this.searchInputValue = "";
         this.elementContainer = "";
         this.menuContainer = "";
+        this.deleteBtnContainer = "";
         this.bookmarksElement = "";
         this.map = ""
         this.favoritesArray
@@ -22,11 +23,11 @@ export default class Favorites{
     */
     saveFavorite(searchInputValue){
         //get fav from localstorage & then parse into array
-        this.searchInputValue = searchInputValue;
+        this.searchInputValue = searchInputValue.toLowerCase();
         this.fetchFavorites();
         //check if the new search already exists in localstorage, if not, add it
         const exists = this.favoritesArray.some(item => 
-            Object.values(item)[0].toLowerCase() === this.searchInputValue.toLowerCase());
+            Object.values(item)[0].toLowerCase() === this.searchInputValue);
         // add as a key value pair.
         if (!exists && searchInputValue!="" && searchInputValue != null) {
             this.favoritesArray.push({ [this.favoritesArray.length]: searchInputValue });
@@ -62,34 +63,87 @@ export default class Favorites{
     ░█▀▄░█▀▀░█░█░█░█░█▀▀░█▀▄░░░█░█░█▀▀░█░█░█░█
     ░▀░▀░▀▀▀░▀░▀░▀▀░░▀▀▀░▀░▀░░░▀░▀░▀▀▀░▀░▀░▀▀▀
     */
-    renderMenu(){
+    renderMenu() {
+        const deleteBtn = './images/icons/close_delete.svg';
         this.menuContainer.innerHTML = 'Saved Location Searches';
-        if (this.favoritesArray.length === 0){
+        //this.menuContainer.setAttribute('id','menuContainer');
+
+        if (this.favoritesArray.length === 0) {
             const msg = document.createElement('div');
-            msg.innerText = "No Saved Locations"
+            msg.innerText = "No Saved Locations";
             this.menuContainer.appendChild(msg);
-        }
-        else{
-            //Attach clickanywheretoclose function to event listener
+        } else {
+            // Attach clickAnywhereToClose function to event listener
             document.addEventListener('click', this.clickAnywhereToClose.bind(this));
 
             this.favoritesArray.forEach(item => {
-                //todo not used yet  - const index = Object.keys(item);
                 const menuItem = document.createElement('div');
+                menuItem.setAttribute('class', 'menuItem');
+                const index = Object.keys(item)[0];
                 const location = capitalizeLocation(Object.values(item)[0]);
-                menuItem.innerText = location;
-                menuItem.classList.add('favorite-location');
+
+                const menuLocation = document.createElement('div');
+                menuLocation.innerText = location;
+                menuLocation.classList.add('menuLocation');
+
+                // Create a delete button
+                const menuDelete = document.createElement('img');
+                menuDelete.setAttribute('src', deleteBtn);
+                menuDelete.setAttribute('alt', 'Delete');
+                menuDelete.setAttribute('id', index);
+                menuDelete.classList.add('delete-btn');
+
+                // Add a click event listener to handle the click on the delete button
+                menuDelete.addEventListener('click', () => {
+                    this.handleDelete(location);
+                });
 
                 // Add a click event listener to handle the click on the menu item
-                menuItem.addEventListener('click', () => {
+                menuLocation.addEventListener('click', () => {
                     loadSearch(location);
                     this.hideMenu();
                 });
-                this.menuContainer.appendChild(menuItem);
+
+                // Append both the menu item and the delete button to the menu container
+                menuItem.appendChild(menuLocation);
+                menuItem.appendChild(menuDelete);
+                this.menuContainer.appendChild(menuItem)
             });
         }
-        showElement(this.menuContainer)
-        this.map.style.zIndex='-1';
+
+        showElement(this.menuContainer);
+        this.map.style.zIndex = '-1';
+    }
+
+
+    /* 
+    ░█▀▄░█▀▀░█░░░█▀▀░▀█▀░█▀▀░░░█▀▀░█▀█░█░█░█▀█░█▀▄░▀█▀░▀█▀░█▀▀
+    ░█░█░█▀▀░█░░░█▀▀░░█░░█▀▀░░░█▀▀░█▀█░▀▄▀░█░█░█▀▄░░█░░░█░░█▀▀
+    ░▀▀░░▀▀▀░▀▀▀░▀▀▀░░▀░░▀▀▀░░░▀░░░▀░▀░░▀░░▀▀▀░▀░▀░▀▀▀░░▀░░▀▀▀
+    */
+    handleDelete(location){
+        console.log(location);
+        location = location.toLowerCase();
+        // Get fav from localstorage & then parse into array
+        this.fetchFavorites();
+        // Check if the location is there
+        console.log(this.favoritesArray);
+    
+        // Find the index of the item with the matching location value
+        const indexToRemove = this.favoritesArray.findIndex(item => Object.values(item)[0].toLowerCase() === location);
+    
+        // Remove the item from the array if found
+        if (indexToRemove !== -1) {
+            this.favoritesArray.splice(indexToRemove, 1);
+        }
+    
+        console.log(this.favoritesArray);
+        localStorage.setItem(this.key, JSON.stringify(this.favoritesArray));
+        //do a quick hide so showFavoritesmenu will handle it right.  
+        // i have code in place to allow bookmarksbutton to toggle it off/on
+        this.renderMenu();
+        this.menuContainer.classList.add('visible');
+        this.menuContainer.classList.remove('visible');
     }
 
     /* 
